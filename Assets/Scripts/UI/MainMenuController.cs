d7e2fed36e6933a78462130bb3ee1f15b73f547d
Sebastian Silva <sebastiansilvabenites@gmail.com>
2024-04-07 00:59:46 -0400

    Add Game Interactions and Animations

---
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using Lean.Transition;
+using Unity.VisualScripting;
+
+namespace UI
+{
+    public class MainMenuController : MonoBehaviour
+    {
+
+        [SerializeField] private Canvas mainMenuCanvas;
+
+        [SerializeField] private Canvas exerciseListCanvas;
+        
+        private void Start()
+        {
+            exerciseListCanvas.GameObject().SetActive(false);
+        }
+
+        public void OpenExerciseList()
+        {
+            mainMenuCanvas.GameObject().SetActive(false);
+            exerciseListCanvas.GameObject().SetActive(true);
+        }
+
+        public void OpenMainMenu()
+        {
+            mainMenuCanvas.GameObject().SetActive(true);
+            exerciseListCanvas.GameObject().SetActive(false);
+        }
+
+        public void ExitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
